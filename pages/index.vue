efdd050b3a4a3ac7fed511a9770ce4189faf1b01
Максим Найдович <max.naid@mail.ru>
2020-07-29 15:43:29 +0500

    add video

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <video
+      class="index_video"
+      loop
+      autoplay
+      poster="main.png"
+      muted="muted"
+      alt="video"
+    >
+      <source src="video.mp4">
+      <source src="video.webm">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.index_video {
+  width: 100%;
+  object-fit: cover;
+  height: 100%;
+  min-height: 600px;
+  max-height: 600px;
+}
+</style>
