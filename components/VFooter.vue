0cd7a4cc517c8007c8d0ce02d56ee54aee1f1afe
Максим Найдович <max.naid@mail.ru>
2020-08-04 10:43:50 +0500

    social added

--- components/VFooter.vue
+++ components/VFooter.vue
@@ -4,9 +4,15 @@
       <div class="v-footer-info">
         <p>&copy; made by Max Naidovich</p>
         <div class="v-footer_social">
-          <a href=""><img src="/icons/telegram.svg" alt=""></a>
-          <a href=""></a>
-          <a href=""></a>
+          <a class="v-footer_social-link" href="">
+            <v-sprite icon="telegram" />
+          </a>
+          <a class="v-footer_social-link" href="">
+            <v-sprite icon="github" />
+          </a>
+          <a class="v-footer_social-link" href="">
+            <v-sprite icon="linkedin" />
+          </a>
         </div>
       </div>
     </div>
@@ -14,24 +20,43 @@
 </template>
 
 <script>
+import VSprite from './VSprite'
 export default {
-  name: 'VFooter'
+  name: 'VFooter',
+  components: VSprite
 }
 </script>
 
 <style scoped>
   .v-footer_background {
-    background-color: var(--c-white);
+    background-color: var(--c-black);
   }
   .v-footer-info {
     font-size: 2rem;
-    color: var(--c-black);
+    color: var(--c-white);
     display: flex;
     justify-content: space-between;
-    align-items: flex-end;
-    padding: 5rem 0 2rem;
+    align-items: center;
+    padding: 2rem 0;
     & p {
       flex-basis: 30%;
+    }
+  }
+  .v-sprite {
+    height: 4rem;
+    width: 4rem;
+    transition: all .3s;
+    fill: var(--c-white);
+
+    &:hover {
+      fill: var(--c-light-gray);
+    }
+  }
+  .v-footer_social-link {
+    display: inline-block;
+    height: 100%;
+    &:not(:last-child) {
+      margin-right: 3rem;
     }
   }
 </style>
