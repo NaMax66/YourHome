fd3da96080696c95e10d5c17a965011909a8a622
Максим Найдович <max.naid@mail.ru>
2020-08-04 14:27:48 +0500

    desktop done

--- components/VFeedback.vue
+++ components/VFeedback.vue
@@ -4,19 +4,41 @@
       <div class="v-feedback_controls">
         <input type="text" placeholder="Your name" class="v-feedback_modal_input">
         <input type="tel" pattern="[0-9]*" placeholder="Your phone" novalidate class="v-feedback_modal_input">
-        <v-button class="v-feedback_action-btn" table-button>
+        <v-button class="v-feedback_action-btn" table-button @click="handleSubmit">
           contact us
         </v-button>
       </div>
     </div>
+    <v-modal v-show="isModalOpen" :is-success-shown="true" @close="closeModal">
+      <template v-slot:thanks>
+        <p class="v-feedback_modal_info">
+          Thank you. We will call you soon!
+        </p>
+      </template>
+    </v-modal>
   </div>
 </template>
 
 <script>
 import VButton from './VButton'
+import VModal from './VModal'
 export default {
   name: 'VFeedback',
-  components: { VButton }
+  components: { VModal, VButton },
+  data: () => ({
+    isModalOpen: false
+  }),
+  methods: {
+    handleSubmit () {
+      this.isModalOpen = true
+      setTimeout(() => {
+        this.isModalOpen = false
+      }, 4000)
+    },
+    closeModal () {
+      this.isModalOpen = false
+    }
+  }
 }
 </script>
 
@@ -43,5 +65,10 @@ export default {
   .v-feedback_controls {
     display: flex;
     align-items: center;
+  }
+  .v-feedback_modal_info {
+    font-size: 2.5rem;
+    line-height: 3.2rem;
+    margin-bottom: 2rem;
   }
 </style>
